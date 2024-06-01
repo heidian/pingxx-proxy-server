36ce7294dc01073ec2cc7c78913a3497f0665b7c
web3nomad.eth <me@web3nomad.eth>
2024-06-01 23:06:21 +0800

    add http request log

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,21 @@ use axum::{
     routing::get,
     Router,
 };
+use tower::ServiceBuilder;
+use tower_http::trace::TraceLayer;
 
 #[tokio::main]
 async fn main() {
+    tracing_subscriber::fmt::init();
     // build our application with a route
     let app = Router::new()
         // `GET /` goes to `root`
-        .route("/", get(root));
+        .route("/", get(root))
+        .layer(
+            ServiceBuilder::new()
+                .layer(TraceLayer::new_for_http())
+                .into_inner(),
+        );
 
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:8002").await.unwrap();
